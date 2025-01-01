f3ddf40ca3a84c23b5387b9e52c8a08bc489657a
Sanket Ghole <106650816+sanketghole@users.noreply.github.com>
2025-01-01 19:37:31 +0530

    Add files via upload

---
+++ CheckMastercourse.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CheckMastercourse.aspx.cs" Inherits="Elearning.CheckMastercourse" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Master Course Details</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container mt-5">
+            <h2>Master Course Details</h2>
+            <div class="form-group">
+                <label for="DropDownList1">Course Master:</label>
+                <asp:DropDownList ID="DropDownList1" runat="server" CssClass="form-control" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                </asp:DropDownList>
+            </div>
+            <div class="form-group">
+                <label for="DropDownList2">Course Name:</label>
+                <asp:DropDownList ID="DropDownList2" runat="server" CssClass="form-control">
+                </asp:DropDownList>
+            </div>
+            <asp:Button ID="Button1" runat="server" CssClass="btn btn-primary" OnClick="Button1_Click" Text="Submit" />
+            <div class="mt-3">
+                <asp:GridView ID="GridView1" runat="server" CssClass="table table-bordered" AutoGenerateColumns="True">
+                </asp:GridView>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CheckMastercourse.aspx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+namespace Elearning
+{
+    public partial class CheckMastercourse : System.Web.UI.Page
+    {
+        private readonly string connectionString = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadMasterCourses();
+            }
+        }
+
+        private void LoadMasterCourses()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT CourseId, CourseName FROM MasterCourses";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        DropDownList1.DataSource = reader;
+                        DropDownList1.DataTextField = "CourseName";
+                        DropDownList1.DataValueField = "CourseId";
+                        DropDownList1.DataBind();
+                    }
+
+                    DropDownList1.Items.Insert(0, new ListItem("--Select Master Course--", ""));
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("<script>alert('Error loading master courses: " + ex.Message + "');</script>");
+                }
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(DropDownList1.SelectedValue))
+            {
+                LoadCourses();
+            }
+        }
+
+        private void LoadCourses()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT CourseId, CourseName FROM Courses WHERE MasterCourseId = @MasterCourseId";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@MasterCourseId", DropDownList1.SelectedValue);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        DropDownList2.DataSource = reader;
+                        DropDownList2.DataTextField = "CourseName";
+                        DropDownList2.DataValueField = "CourseId";
+                        DropDownList2.DataBind();
+                    }
+
+                    DropDownList2.Items.Insert(0, new ListItem("--Select Course--", ""));
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("<script>alert('Error loading courses: " + ex.Message + "');</script>");
+                }
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            LoadGridView();
+        }
+
+        private void LoadGridView()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = @"
+                        SELECT 
+                            c.CourseId AS [ID],
+                            mc.CourseName AS [MasterCourse Name],
+                            c.CourseName AS [Course Name],
+                            (SELECT COUNT(*) FROM Topics t WHERE t.CourseId = c.CourseId) AS [No. of Topics]
+                        FROM Courses c
+                        INNER JOIN MasterCourses mc ON c.MasterCourseId = mc.CourseId
+                        WHERE (@MasterCourseId IS NULL OR mc.CourseId = @MasterCourseId)
+                          AND (@CourseId IS NULL OR c.CourseId = @CourseId)";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    cmd.Parameters.AddWithValue("@MasterCourseId", string.IsNullOrEmpty(DropDownList1.SelectedValue) ? (object)DBNull.Value : DropDownList1.SelectedValue);
+                    cmd.Parameters.AddWithValue("@CourseId", string.IsNullOrEmpty(DropDownList2.SelectedValue) ? (object)DBNull.Value : DropDownList2.SelectedValue);
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("<script>alert('Error loading grid data: " + ex.Message + "');</script>");
+                }
+            }
+        }
+    }
+}
